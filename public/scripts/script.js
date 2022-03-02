3474a6730c47687b75c202a7f216cde8c1d48bde
EvilWeasel <38261180+EvilWeasel@users.noreply.github.com>
2022-03-02 16:34:04 +0100

    feierabend; todo: confirm on delete implementieren; mvp done, front-end

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-    
-});
+    let articleCards = $('.cards')
+    let query
+    $('#query').keyup(function() {
+        query = $(this).val();
+        articleCards.each(function() {
+            if ($(this).text().search(new RegExp(query, "i")) < 0) {
+                $(this).fadeOut();
+            } else {
+                $(this).fadeIn();
+            }
+        })
+    })
+})
+
